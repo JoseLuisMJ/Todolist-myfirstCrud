b1789470ec26364c01b96e9edf53519630f9a746
JoseLuisMJ <trepa01@hotmail.com>
2023-09-28 18:39:01 +0200

    add javascript

--- app.js
+++ app.js
@@ -1,55 +1,150 @@
-let form = document.getElementById("form");
-let input = document.getElementById("input");
-let msg = document.getElementById("msg");
-let posts = document.getElementById("posts");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("button clicked");
+const form = document.querySelector('form');
+const taskList = document.querySelector('#task-list');
 
-  formValidation();
-});
+function addTask(task){
+    const listItem = document.createElement('li')
+    listItem.innerHTML = `<span>${task}</span><button>Delete</button>`;
+    taskList.appendChild(listItem)
+}
 
-let formValidation = () => {
-  if (input.value === "") {
-    msg.innerHTML = "Post cannot be blank";
-    console.log("failure");
-  } else {
-    console.log("successs");
-    msg.innerHTML = "";
-    acceptData();
-  }
-};
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const input = document.querySelector('#task-input');
+    const task = input.value;
+    addTask(task);
+    input.value = ''
+})
 
-let data = {};
+taskList.addEventListener('click', (event)=>{
+    if(event.target.tagName === 'BUTTON'){
+        const listItem = event.target.parentElement
+        taskList.removeChild(listItem);
+    }
+})
+// let persons = [];
 
-let acceptData = () => {
-  data["text"] = input.value;
-  console.log(data);
-  createPost();
-};
+// function createPerson(name) {
+//     let person = {
+//         id: Date.now(),
+//         name: name
+//     };
 
-let createPost = () => {
-  posts.innerHTML += `
-  <div>
-    <p>${data.text}</p>
-    <span class="options">
-      <i onClick="editPost(this)" class="fas fa-edit"></i>
-      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-    </span>
-  </div>
-  `;
-  input.value = "";
-};
+//     persons.push(person);
 
-let deletePost = (e) => {
-  e.parentElement.parentElement.remove();
-};
-<i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+//     document.getElementById('person-form').reset();
 
-let editPost = (e) => {
-  input.value = e.parentElement.previousElementSibling.innerHTML;
-  e.parentElement.parentElement.remove();
-};
+//     displayPersons();
+// }
 
-<i onClick="editPost(this)" class="fas fa-edit"></i>
+// function displayPersons() {
+//     document.getElementById('person-list').innerHTML = '';
+
+//     for (let i = 0; i < persons.length; i++) {
+//         let person = persons[i];
+
+//         let listItem = document.createElement('li');
+//         listItem.textContent = person.name;
+
+//         let editButton = document.createElement('button');
+//         editButton.textContent = 'Edit';
+//         editButton.addEventListener('click', function() {
+//             editPerson(person.id);
+//         });
+
+//         let deleteButton = document.createElement('button');
+//         deleteButton.textContent = 'Delete';
+//         deleteButton.addEventListener('click', function() {
+//             deletePerson(person.id);
+//         });
+
+//         listItem.appendChild(editButton);
+//         listItem.appendChild(deleteButton);
+
+//         document.getElementById('person-list').appendChild(listItem);
+//     }
+// }
+
+// function editPerson(id) {
+//     let newName = prompt('Enter a new name:');
+//     if (newName) {
+//         let person = persons.find(person => person.id === id);
+//         if (person) {
+//             person.name = newName;
+//             displayPersons();
+//         }
+//     }
+// }
+
+// function deletePerson(id) {
+//     let index = persons.findIndex(person => person.id === id);
+//     if (index !== -1) {
+//         persons.splice(index, 1);
+//         displayPersons();
+//     }
+// }
+
+// document.getElementById('person-form').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     let name = document.getElementById('name-input').value;
+//     createPerson(name);
+// });
+
+// displayPersons();
+
+
+// let form = document.getElementById("form");
+// let input = document.getElementById("input");
+// let msg = document.getElementById("msg");
+// let posts = document.getElementById("posts");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   console.log("button clicked");
+
+//   formValidation();
+// });
+
+// let formValidation = () => {
+//   if (input.value === "") {
+//     msg.innerHTML = "Post cannot be blank";
+//     console.log("failure");
+//   } else {
+//     console.log("successs");
+//     msg.innerHTML = "";
+//     acceptData();
+//   }
+// };
+
+// let data = {};
+
+// let acceptData = () => {
+//   data["text"] = input.value;
+//   console.log(data);
+//   createPost();
+// };
+
+// let createPost = () => {
+//   posts.innerHTML += `
+//   <div>
+//     <p>${data.text}</p>
+//     <span class="options">
+//       <i onClick="editPost(this)" class="fas fa-edit"></i>
+//       <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+//     </span>
+//   </div>
+//   `;
+//   input.value = "";
+// };
+
+// let deletePost = (e) => {
+//   e.parentElement.parentElement.remove();
+// };
+// <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+
+// let editPost = (e) => {
+//   input.value = e.parentElement.previousElementSibling.innerHTML;
+//   e.parentElement.parentElement.remove();
+// };
+
+// <i onClick="editPost(this)" class="fas fa-edit"></i>
